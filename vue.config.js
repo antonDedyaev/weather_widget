2eaf75aaa83b8a2c48209594de068fcc69c74a10
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-08-14 15:26:42 +0300

    create project and remove trash files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/styles/main.scss";
+        `,
+      },
+    },
+  },
   transpileDependencies: true,
 });
